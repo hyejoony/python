70b81d87cfa161a1d2904ac588276be40a49cec0
hyejoony <137747849+hyejoony@users.noreply.github.com>
2023-07-05 18:20:44 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
@@ -1,10 +1,3 @@
 n = int(input())
-for i in range(n,0,-1):
-    if i == 1:
-        print(" "*(n-i) + "*"*(2*i-1),end='')
-    elif i != 1:
-        print(" "*(n-i) + "*"*(2*i-1))
-
-        
-
-    
+for i in range(0,n):
+    print(" "*i + "*"*(2*n-1-(2*i)),sep="")
