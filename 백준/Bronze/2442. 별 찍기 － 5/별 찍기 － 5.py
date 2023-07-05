5cc562a88c849d25f256550dedbb3707a9c2f792
hyejoony <137747849+hyejoony@users.noreply.github.com>
2023-07-05 16:45:07 +0900

    Update 별 찍기 － 5.py(2)

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -1,3 +1,10 @@
 n =int(input())
 for i in range(1,n+1):
     print(" "*(n-i)+ "*"*(2*i-1))
+
+a = int(input())
+for i in range(1,a+1):
+    if a != i:
+        print(' '*(a-i) + '*'*(2*i -1))
+    elif a == i:
+        print(' '*(a-i) + '*'*(2*i -1), end='')#마지막 줄만 엔터를 치지 않겠다
