17fe38c74cd1ffb66b22c58f75ca27a7c60acb58
hyejoony <137747849+hyejoony@users.noreply.github.com>
2023-07-06 19:18:09 +0900

    Update 별 찍기 － 6.py

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
@@ -1,13 +1,9 @@
-n = int(input())
-for i in range(0,n):
-    print(" "*i + "*"*(2*n-1-(2*i)),sep="")
-
 
 n = int(input())
+for i in range(n,0,-1):
+    if i == 1:
+        print(" "*(n-i) + "*"*(2*i-1),end='')
+    elif i != 1:
+        print(" "*(n-i) + "*"*(2*i-1))
 
-for i in range(n, 0, -1):
-    # 왼쪽 공백 출력
-    print(' ' * (n - i), end='')
-    # 별 출력
-    print('*' * (2 * i - 1))
     
