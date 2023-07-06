c1728f90688d6555841c57a987d038d239b270d4
hyejoony <137747849+hyejoony@users.noreply.github.com>
2023-07-06 20:59:19 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+
+n = int(input())
+for i in range(1,n+1):
+    print(("*"*i + " "*(n-i))+(" "*(n-i)+ ("*"*i)))
+for j in range(n-1,0,-1):
+    if j ==1:
+        print(("*"*j + " "*(n-j))+(" "*(n-j)+ ("*"*j)),end='')
+    elif j != 1:
+        print(("*"*j + " "*(n-j))+(" "*(n-j)+ ("*"*j)))
