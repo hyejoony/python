0798830dbff3add0f6f8de2a55325ed6007e8dec
hyejoony <137747849+hyejoony@users.noreply.github.com>
2023-08-07 23:41:14 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,6 +1,4 @@
-n = int(input()) #정수 개수
-lst = list(map(int, input().split())) #리스트 입력
-v = int(input()) #추출 요소
-
-
+N = int(input())
+lst = list(map(int,input().split()))
+v = int(input())
 print(lst.count(v))
