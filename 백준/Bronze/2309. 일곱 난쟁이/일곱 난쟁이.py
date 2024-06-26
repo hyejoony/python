a49ac45abbc987098881b070f99e8f72511f5fdc
Hye-joon Jeon <137747849+hyejoony@users.noreply.github.com>
2024-06-26 16:36:50 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,19 +1,34 @@
-
-#빈 배열을 하나 준비해서 9개의 값을 입력받아 넣어줌
-#첫째줄에 값 0 개를 9개 줄로 입력하고 싶을 때 
-n = []
+lst =[]# 난쟁이 담을 리스트
+end_loop = True
 for _ in range(9):
-    n.append(int(input()))
-#한 배열 내에서 값을 순차적으로 비교할 때 = 구구단과 유사
-n.sort()
-result = sum(n)
+    K = int(input())
+    lst.append(K)
 
-for i in range(9):
-    for j in range(i+1,9):
-        if result - n[i] - n[j] == 100:
-            for k in range(9):
-                if k == i or k== j:
-                    continue
-                else:
-                    print(n[k])
-            exit()
+for i in range(len(lst)-1): #0~7까지
+    
+    for j in range(i, len(lst)-1): #0~7까지
+        
+        a = lst.pop(i)
+        b = lst.pop(j)
+
+        if sum(lst) == 100:
+            lst.sort()
+            for z in lst:
+                print(z)
+            
+            end_loop = False
+            break
+
+        else:
+
+            lst.insert(i,a)
+            lst.insert(j+1,b)
+
+    if end_loop == False:
+        break
+
+    
+
+
+
+    
