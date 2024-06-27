8b49605f49ed4bba1949d3323166a6ec2d9fe5c9
Hye-joon Jeon <137747849+hyejoony@users.noreply.github.com>
2024-06-27 17:38:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,22 +1,60 @@
-N, M = map(int,input().split())
-#N*M 행렬
-mat1 =[[0 for _ in range(M)] for _ in range(N)]
-mat2 =[[0 for _ in range(M)] for _ in range(N)]
-mat3 =[[0 for _ in range(M)] for _ in range(N)]
-#[[0,0,0],[0,0,0],[0,0,0]]
-#행렬문제는 이중 for문을 돌림
-for i in range(N):
-    cols = list(map(int, input().split(' ')))
-    for j in range(M):
-        mat1[i][j] =cols[j] #0,0좌표
-for i in range(N):
-    cols  = list(map(int, input().split(' ')))
-    for j in range(M):
-        mat2[i][j] =cols[j]
-for i in range(N):
-    for j in range(M):
-        mat3[i][j] = mat1[i][j]+mat2[i][j]
-for i in range(N):
-    for j in range(M):
-        print(mat3[i][j],end=' ')
+N,M = map(int,input().split())
+
+matrix_1 = [[0 for i in range(M)] for _ in range(N)]
+#행렬 a 초기화
+matrix_2 = [[0 for i in range(M)] for _ in range(N)]
+#행렬 b 초기화
+matrix_3 = [[0 for i in range(M)] for _ in range(N)]
+#a+b 계산 행렬 초기화
+
+#matrix
+#[[0,0,0]
+#[0,0,0]
+#[0,0,0]]
+
+#행렬a
+for i in range(N): #0,1,2
+    oneso = list(map(int, input().split()))
+    #oneso = [1,1,1]
+    for j in range(M): 
+        # i=0일때 j = 0,1,2
+        matrix_1[i][j] = oneso[j]
+        #matrix_1[0][0] = oneso[0]
+        # j =1일때
+        #matrix_1[0][1] = oneso[1]
+        # j =2일때
+        #matrix_1[0][2] = oneso[2]
+
+#행렬b
+for a in range(N):
+    oneso_2 = list(map(int, input().split()))
+    for b in range(M):
+        matrix_2[a][b] = oneso_2[b]
+
+
+#행렬a+b
+for z in range(N): 
+    for y in range(M):
+        matrix_3[z][y] = matrix_1[z][y] + matrix_2[z][y]
+
+#답 출력
+for x in range(N):
+    for z in range(M):
+        print(matrix_3[x][z], end=' ')
+    #x = 0, z = 0,1,2 한바퀴 돌고(출력하고)
     print()
+    
+    
+
+
+        
+
+
+
+
+
+
+
+
+
+
