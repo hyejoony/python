95f2bfcb4813ea8e7346a38ed8cff8e64dba5fe3
Hye-joon <137747849+hyejoony@users.noreply.github.com>
2024-10-22 22:36:01 +0900

    [level 2] Title: 모음 사전, Time: 1.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 1.79 ms
+메모리: 10.2 MB, 시간: 1.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 21:31:19
+2024년 10월 22일 22:35:58
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,21 +1,30 @@
 def solution(word):
-    words = []
-    
-    def recur(cur_string):
-        if cur_string:  # 빈 문자열이 아닌 경우에만 추가
-            words.append(cur_string)
-        
-        if len(cur_string) == 5:
-            return
-        
-        for next_char in 'AEIOU':
-            recur(cur_string + next_char)
-    
-    recur("")
-    return words.index(word) + 1
+    alphabets = ['A', 'E', 'I', 'O', 'U']
+    global answer 
+    answer = 0 
 
-# # 테스트
-# print(solution("AAAAE"))  # 6
-# print(solution("AAAE"))   # 10
-# print(solution("I"))      # 1563
-# print(solution("EIO"))    # 1189
+    def dfs(string):
+        global answer 
+        answer += 1  # 새로운 단어를 만들 때마다 카운트 증가
+        
+        # 목표 단어를 찾으면 True 반환
+        if string == word:
+            return True
+        
+        # 단어 길이가 5가 되면 더 이상 확장하지 않고 False 반환
+        if len(string) == 5:
+            return False
+        
+        # 현재 단어에 각 모음을 추가하여 새로운 단어 생성
+        for alpha in alphabets:
+            # 새로운 단어로 재귀 호출, True가 반환되면 목표 단어를 찾은 것
+            if dfs(string + alpha) == True:
+                return True 
+        
+        # 모든 모음을 시도해도 목표 단어를 못 찾으면 False 반환
+        return False
+
+    # 각 모음으로 시작하는 단어에 대해 DFS 수행
+    for alpha in alphabets:
+        if dfs(alpha) == True:
+            return answer
