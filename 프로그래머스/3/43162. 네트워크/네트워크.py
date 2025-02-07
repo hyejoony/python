73477ab25dc07c911f5c2864f4857a78c878f2d3
Hye-joon <137747849+hyejoony@users.noreply.github.com>
2025-02-07 22:35:42 +0900

    [level 3] Title: 네트워크, Time: 0.88 ms, Memory: 7.71 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+G = [[] for _ in range(101)]
+vis = [False] * 101
+
+def bfs(node):
+
+    # initialization 
+    q = deque([node])
+    vis[node] = True
+
+    # main logic : BFS 
+    while q:
+        cur = q.popleft()
+        for nxt in G[cur]:
+            if not vis[nxt]:
+                vis[nxt] = True
+                q.append(nxt)
+
+    return vis 
+
+def solution(n, computers):
+    answer = 0
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j]:
+                G[i].append(j)
+                G[j].append(i)
+
+    for i in range(n):
+        if not vis[i]:
+            bfs(i)
+            answer+=1
+
+    return answer
